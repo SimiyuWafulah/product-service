5d8ae736b2943da1bb7074b7a33235629e8fadcb
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-07-28 21:09:22 +0300

    api routes for products

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import productRouter from './routes/product.route.js'
 import dotenv from 'dotenv'
 dotenv.config();
 
 const app = express()
-app.use(express.json())
+app.use(express.json());
+app.use('/api', productRouter);
 
 //connecting to db
 mongoose.connect(process.env.mongo_url).then(() => {
